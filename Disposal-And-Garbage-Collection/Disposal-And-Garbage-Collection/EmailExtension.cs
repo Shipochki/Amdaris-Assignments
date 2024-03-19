b1dcf2cb7a5c1d86e0ecaa08ac91e7598d60f635
Shipochki <121862582+Shipochki@users.noreply.github.com>
2024-03-19 13:32:36 +0200

    Created email sender

---
+++ Disposal-And-Garbage-Collection/Disposal-And-Garbage-Collection/EmailExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace Disposal_And_Garbage_Collection
+{
+	public static class EmailExtension
+	{
+		public static void SendEmail(string subject, string body, string reciverEmail)
+		{
+			string password = "tdjo oqqu fbbk vhpp";
+			string emailSender = "travelbuddies.amdaris@gmail.com";
+
+			SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+			smtpClient.EnableSsl = true;
+			smtpClient.UseDefaultCredentials = false;
+			smtpClient.Credentials = new NetworkCredential(emailSender, password);
+
+			MailMessage mailMessage = new MailMessage();
+			mailMessage.From = new MailAddress(emailSender);
+			mailMessage.To.Add(reciverEmail);
+			mailMessage.Subject = subject;
+			mailMessage.Body = body;
+			smtpClient.Send(mailMessage);
+		}
+	}
+}
---
+++ Disposal-And-Garbage-Collection/Disposal-And-Garbage-Collection/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Disposal_And_Garbage_Collection
+{
+	using static EmailExtension;
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Email:");
+            string? reciverEmail = Console.ReadLine();
+
+			try
+			{
+				SendEmail(subject: "Subscribing to the newsletter", body: "Thank you! For subscribing to our newsletter!", reciverEmail);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+	}
+}
